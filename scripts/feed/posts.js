8feac79705510f4c7b6ff62a95ada992e0563977
eskiltorset <44784426+eskiltorset@users.noreply.github.com>
2023-10-09 15:46:43 +0200

    user can now createPost()

--- scripts/feed/posts.js
+++ scripts/feed/posts.js
@@ -24,61 +24,57 @@ async function fetchPosts(url) {
 
         const posts_section = document.getElementById('posts_section');
 
-        if(json[i].title != '' || json[i].body != ''){
-            /*posts_section.innerHTML += `
-            <div class="post p-4 border-bottom border-top">
-                <div class="w-25 pb-2">
-                    <img src="/images/avatar.avif" class="w-25 img-fluid rounded-circle float start">
-                </div>
-                <h5 class="username">@${json[i].author.name}</h5>
-                <h4 class="title">${json[i].title}</h4>
-                <p class="description">${json[i].body}</p>
-                <img src="" alt="${json[i].title}" class="w-25" id="postImg">
-            </div>
-        `;*/
+        if(json[i].title && json[i].body){
 
-        const postDiv = document.createElement("div");
-        postDiv.classList.add("post", "p-4", "border-bottom", "border-top")
+            const postDiv = document.createElement("div");
+            postDiv.classList.add("post", "p-4", "border-bottom", "border-top")
 
-        const avatarDiv = document.createElement("div");
-        avatarDiv.classList.add("w-25", "pb-2");
+            const avatarDiv = document.createElement("div");
+            avatarDiv.classList.add("w-25", "pb-2");
 
-        const avatarImg = document.createElement("img"); 
-        avatarImg.classList.add("w-25", "img-fluid", "rounded-circle", "float", "start");
-        avatarImg.alt = "Avatar Image";
-        avatarImg.src = "/images/avatar.avif";
+            const avatarImg = document.createElement("img"); 
+            avatarImg.classList.add("w-25", "img-fluid", "rounded-circle", "float", "start");
+            avatarImg.alt = "Avatar Image";
+            avatarImg.src = "/images/avatar.avif";
 
-        if(json[i].author.avatar){
-            avatarImg.src = json[i].author.avatar;
-        }
+            if(json[i].author.avatar){
+                avatarImg.src = json[i].author.avatar;
+            }
 
-        const username = document.createElement("h5");
-        username.classList.add("username");
-        username.innerHTML = `@${json[i].author.name}`;
+            const username = document.createElement("h5");
+            username.classList.add("username");
+            username.innerHTML = `@${json[i].author.name}`;
 
-        const title = document.createElement("h4");
-        title.classList.add("title", "fw-bolder");
-        title.innerHTML = json[i].title;
-        
-        const description = document.createElement("p");
-        description.classList.add("description");
-        description.innerHTML = json[i].body;
+            const title = document.createElement("h4");
+            title.classList.add("title", "fw-bolder");
+            title.innerHTML = json[i].title;
+            
+            const description = document.createElement("p");
+            description.classList.add("description");
+            description.innerHTML = json[i].body;
 
-        posts_section.appendChild(postDiv);
-        postDiv.appendChild(avatarDiv);
-        avatarDiv.appendChild(avatarImg);
-        postDiv.appendChild(username);
-        postDiv.appendChild(title);
-        postDiv.appendChild(description);
+            const comments = document.createElement("h8");
+            avatarDiv.classList.add("comments");
+            comments.innerHTML = `Comments: ${json[i].comments.length}`;
 
-        if(json[i].media){
-            const postImg = document.createElement("img"); 
-            postImg.classList.add("w-25");
-            postImg.alt = "Post Image";
-            postImg.src = json[i].media;
+            posts_section.appendChild(postDiv);
+            postDiv.appendChild(avatarDiv);
+            avatarDiv.appendChild(avatarImg);
+            postDiv.appendChild(username);
+            postDiv.appendChild(title);
+            postDiv.appendChild(description);
 
-            postDiv.appendChild(postImg);
-        }
+            if(json[i].media){
+                const postImg = document.createElement("img"); 
+                postImg.classList.add("w-25");
+                postImg.alt = "Post Image";
+                postImg.src = json[i].media;
+
+                postDiv.appendChild(postImg);
+                
+            }
+
+            postDiv.appendChild(comments);
             
         }
 
@@ -92,6 +88,73 @@ async function fetchPosts(url) {
     }
   }
   
-  const fetchPosts_URL = `${API_BASE_URL}/api/v1/social/posts?limit=10&offset=125&_comments=true&_author=true&_reactions=true&_count=true`;
+  const fetchPosts_URL = `${API_BASE_URL}/api/v1/social/posts?limit=15&offset=125&_comments=true&_author=true&_reactions=true&_count=true`;
   
   fetchPosts(fetchPosts_URL);
+
+
+async function createPost(url, postData) {
+
+    try {
+        console.log(url);
+        const token = localStorage.getItem("accessToken");
+        console.log(token);
+        const fetchOptions = {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`
+          },
+          body: JSON.stringify(postData),
+        }
+    
+        const response = await fetch(url, fetchOptions);
+        console.log(response);
+        const json = await response.json();
+        console.log(json);
+
+        fetchPosts(fetchPosts_URL);
+  
+    }
+    
+    catch(error) {
+        console.log(error);
+    }
+}
+
+const postForm = document.getElementById("postForm");
+
+postForm.addEventListener("submit", async (event) => {
+
+    event.preventDefault();
+
+    const titleInput = document.getElementById("title_input").value;
+    const bodyInput = document.getElementById("description_input").value;
+    const mediaInput = document.getElementById("media_input").value;
+
+    if(titleInput.length > 0) {
+
+        try {
+
+            let userPost = {
+                title: titleInput,
+                body: bodyInput,
+                media: mediaInput
+            };
+
+            const createPosts_URL = `${API_BASE_URL}/api/v1/social/posts`;
+
+            await createPost(createPosts_URL, userPost);
+        }
+
+        catch(error) {
+            console.log(error);
+        }
+    }
+
+    else {
+        alert("You need to write a title!")
+    }
+});
+
+
